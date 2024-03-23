12ba2ae61a8c5c12f5dae1800af65cc99da72e7c
fcdigitalsolutions <63717171+fcdigitalsolutions@users.noreply.github.com>
2024-03-23 18:44:51 -0300

    Delete src directory

--- src/componentes/BtnMap/index.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./style.module.css";
-
-export default function BtnMap() {
-  return <button className={styles.btn}>Abrir</button>;
-}
--- src/componentes/BtnSend/index.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./style.module.css";
-
-export default function BtnSend() {
-  return <button className={styles.btn}>Enviar Dados</button>;
-}
